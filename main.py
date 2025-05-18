a496d29e9429f3217cd79b982ff464ccef35d84e
Asna Assalam <167699502+asnaassalam@users.noreply.github.com>
2025-05-18 11:31:53 +0530

    Added user registration function

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ def display_message(user_text):
     print("|" + " " * padding + text + " " * (box_width - len(text) - padding - 2) + "|")
     print("=" * box_width)
 
+# Stores existing users and their passwords (in-memory storage for now)
+user_details = {
+    "demo_user": "demo123"
+}
+
+# Registration function
+def registration():
+    print("\n")
+    display_message("Register Here!")
+    while True:
+        username = input("Username: ")
+        if username in user_details:
+            print("User already exists. Please enter another username.\n")
+        else:
+            password = input("Password: ")
+            user_details[username] = password
+            print("Registration Successful!\n")
+            break
+            
 # Main function that drives the CLI menu
 def main():
     display_message("Welcome to PyAuth System")
@@ -22,7 +41,7 @@ def main():
         else:
             match main_menu:
                 case 1:
-                    print("Registration function comig soon")
+                    registration()
                 case 2:
                     print("Login function coming soon")
                 case 3:
